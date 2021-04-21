b13aad044cb4060beade4f5ced67197e37f416b6
Ronan <liruonan@hotmail.com>
2021-04-21 23:11:46 +0800

    chore: init

---
+++ src/extension.ts
@@ -0,0 +1,10 @@
+import * as vscode from 'vscode';
+import commandRegister from './commandRegister';
+
+export function activate(context: vscode.ExtensionContext) {
+	commandRegister(context);
+	// to do
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() { }
